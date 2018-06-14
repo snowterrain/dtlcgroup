9a4918d8b88f19686f770f2f7ba1c787dd76e8a9
snowterrain <gpavan99@gmail.com>
2018-06-13 23:23:36 -0400

    interval set

--- server/api.js
+++ server/api.js
@@ -5,8 +5,10 @@ var fs      = require('fs');
 var mustache = require('mustache');
 const path = require("path");
 
-
-
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 //Send Mail fnctionality
 sendMail = function (toEmail, subjectLine, content) {
