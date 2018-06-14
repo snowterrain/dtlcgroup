e5f668bebd94884cab0a30ec0713764e5d4ae29d
snowterrain <gpavan99@gmail.com>
2018-06-13 23:29:40 -0400

    update http refresh

--- server/api.js
+++ server/api.js
@@ -7,8 +7,8 @@ const path = require("path");
 
 var http = require("http");
 setInterval(function() {
-    http.get("http://<your app name>.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
+    http.get("https://dtlcgroup.herokuapp.com");
+}, 900000); // every 15 minutes (300000)
 
 //Send Mail fnctionality
 sendMail = function (toEmail, subjectLine, content) {
