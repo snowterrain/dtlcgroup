90e7918e7e7e2bd93740c1980e5afe0181f1a9b0
snowterrain <gpavan99@gmail.com>
2018-09-24 23:27:49 -0400

    updated

--- dist/dtlcgroup/polyfills.js
+++ dist/dtlcgroup/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\java\siva\dtlcgroup\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\java\siva\newdtlc\dtlcgroup\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- dist/dtlcgroup/styles.js
+++ dist/dtlcgroup/styles.js
@@ -662,10 +662,10 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\java\siva\dtlcgroup\node_modules\font-awesome\scss\font-awesome.scss */"./node_modules/font-awesome/scss/font-awesome.scss");
-__webpack_require__(/*! C:\java\siva\dtlcgroup\node_modules\angular-bootstrap-md\scss\bootstrap\bootstrap.scss */"./node_modules/angular-bootstrap-md/scss/bootstrap/bootstrap.scss");
-__webpack_require__(/*! C:\java\siva\dtlcgroup\node_modules\angular-bootstrap-md\scss\mdb-free.scss */"./node_modules/angular-bootstrap-md/scss/mdb-free.scss");
-module.exports = __webpack_require__(/*! C:\java\siva\dtlcgroup\src\styles.scss */"./src/styles.scss");
+__webpack_require__(/*! C:\java\siva\newdtlc\dtlcgroup\node_modules\font-awesome\scss\font-awesome.scss */"./node_modules/font-awesome/scss/font-awesome.scss");
+__webpack_require__(/*! C:\java\siva\newdtlc\dtlcgroup\node_modules\angular-bootstrap-md\scss\bootstrap\bootstrap.scss */"./node_modules/angular-bootstrap-md/scss/bootstrap/bootstrap.scss");
+__webpack_require__(/*! C:\java\siva\newdtlc\dtlcgroup\node_modules\angular-bootstrap-md\scss\mdb-free.scss */"./node_modules/angular-bootstrap-md/scss/mdb-free.scss");
+module.exports = __webpack_require__(/*! C:\java\siva\newdtlc\dtlcgroup\src\styles.scss */"./src/styles.scss");
 
 
 /***/ })
